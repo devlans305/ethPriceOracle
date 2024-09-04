44e147fd95cbde07b39f42b73e8ffa0731be9225
Evance Onyango <devlans305@gmail.com>
2024-09-04 05:39:03 +0300

    updated caller callbacks and modifier

--- caller/CallerContract.sol
+++ caller/CallerContract.sol
@@ -1,14 +1,32 @@
 pragma solidity 0.5.0;
 import "./EthPriceOracleInterface.sol";
-// 1. import the contents of "openzeppelin-solidity/contracts/ownership/Ownable.sol"
-contract CallerContract { // 2. Make the contract inherit from `Ownable`
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+contract CallerContract is Ownable {
+    uint256 private ethPrice;
     EthPriceOracleInterface private oracleInstance;
     address private oracleAddress;
+    mapping(uint256=>bool) myRequests;
     event newOracleAddressEvent(address oracleAddress);
-    // 3. On the next line, add the `onlyOwner` modifier to the `setOracleInstanceAddress` function definition
-    function setOracleInstanceAddress (address _oracleInstanceAddress) public {
+    event ReceivedNewRequestIdEvent(uint256 id);
+    event PriceUpdatedEvent(uint256 ethPrice, uint256 id);
+    function setOracleInstanceAddress (address _oracleInstanceAddress) public onlyOwner {
       oracleAddress = _oracleInstanceAddress;
       oracleInstance = EthPriceOracleInterface(oracleAddress);
-      // 4. Fire `newOracleAddressEvent`
+      emit newOracleAddressEvent(oracleAddress);
+    }
+    function updateEthPrice() public {
+      uint256 id = oracleInstance.getLatestEthPrice();
+      myRequests[id] = true;
+      emit ReceivedNewRequestIdEvent(id);
+    }
+    function callback(uint256 _ethPrice, uint256 _id) public onlyOracle {
+      require(myRequests[_id], "This request is not in my pending list.");
+      ethPrice = _ethPrice;
+      delete myRequests[_id];
+      emit PriceUpdatedEvent(_ethPrice, _id);
+    }
+    modifier onlyOracle() {
+      require(msg.sender == oracleAddress, "You are not authorized to call this function.");
+      _;
     }
 }
